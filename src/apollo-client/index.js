5d8e2bb5fd01d22afdd74bc15915afd4151c1202
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-02 11:21:46 +0100

    create and mount client on react app

---
+++ src/apollo-client/index.js
@@ -0,0 +1,7 @@
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: process.env.API_URL
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ApolloProvider } from 'react-apollo';
 import App from './components/app';
+import client from './apollo-client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+);
 
 module.hot.accept();
