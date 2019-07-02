434b4cb9f72bbb61b18ee922ec7426341616f5d3
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-02 11:21:18 +0100

    configure webpack for env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.jsx']
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({ template: './src/index.html' })
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  }
+module.exports = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.mjs', '.js', '.jsx']
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new webpack.DefinePlugin(envKeys)
+    ],
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    }
+  };
 };
