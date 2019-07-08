ad2138fdb53fae23eb5c73079ab7f91cbc5c16ac
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-08 21:52:11 +0100

    update apollo client

--- src/apollo-client/index.js
+++ src/apollo-client/index.js
@@ -1,7 +1,15 @@
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: process.env.API_URL
+  uri: process.env.API_URL,
+  request: operation => {
+    const token = localStorage.getItem('token') || '';
+    operation.setContext({
+      headers: {
+        authorization: token
+      }
+    });
+  }
 });
 
 export default client;
