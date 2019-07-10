17798d8fb080e31b0fbcba626c352bcf6b862408
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-10 10:03:36 +0100

    render document list in dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,10 +1,99 @@
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
+import { UserContext } from '../contexts';
+import DocumentList from './document-list';
+
+const filterDocuments = (docs, type) => {
+  return docs.filter(doc => doc.access === type);
+};
 
 function Dashboard() {
+  const { myDocuments } = useContext(UserContext);
+
+  const publicDocuments = myDocuments && filterDocuments(myDocuments, 'PUBLIC');
+  const privateDocuments =
+    myDocuments && filterDocuments(myDocuments, 'PRIVATE');
+  const roleDocuments = myDocuments && filterDocuments(myDocuments, 'ROLE');
+
   return (
-    <div>
-      <h1>This is the dashboard</h1>
-    </div>
+    <Fragment>
+      {myDocuments && (
+        <Fragment>
+          <ul
+            className="nav nav-tabs justify-content-center"
+            id="docsTab"
+            role="tablist"
+          >
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                id="publicDocs-tab"
+                data-toggle="tab"
+                href="#public"
+                role="tab"
+                aria-controls="public"
+                aria-selected="true"
+              >
+                PUBLIC DOCUMENTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="privateDocs-tab"
+                data-toggle="tab"
+                href="#private"
+                role="tab"
+                aria-controls="private"
+                aria-selected="false"
+              >
+                PRIVATE DOCUMENTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                id="roleDocs-tab"
+                data-toggle="tab"
+                href="#role"
+                role="tab"
+                aria-controls="role"
+                aria-selected="false"
+              >
+                ROLE DOCUMENTS
+              </a>
+            </li>
+          </ul>
+          <div className="tab-content" id="docsTabContent">
+            <div
+              className="tab-pane fade show active"
+              id="public"
+              role="tabpanel"
+              aria-labelledby="publicDocs-tab"
+            >
+              <DocumentList documents={publicDocuments} />
+            </div>
+            <div
+              className="tab-pane fade"
+              id="private"
+              role="tabpanel"
+              aria-labelledby="privateDocs-tab"
+            >
+              {privateDocuments && (
+                <DocumentList documents={privateDocuments} />
+              )}
+            </div>
+            <div
+              className="tab-pane fade"
+              id="role"
+              role="tabpanel"
+              aria-labelledby="roleDocs-tab"
+            >
+              {roleDocuments && <DocumentList documents={roleDocuments} />}
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 }
 
---
+++ src/components/document-list.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const renderContentPart = content => {
+  return content.length > 50 ? `${content.slice(0, 60)}...` : content;
+};
+
+function DocumentList({ documents }) {
+  return (
+    <div className="row mt-5">
+      {documents.map(document => (
+        <div className="col-sm-4 mb-2" key={document.id}>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{document.title}</h5>
+              <p className="card-text">{renderContentPart(document.content)}</p>
+              <a href="#" className="btn btn-primary">
+                See more
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+DocumentList.propTypes = {
+  documents: PropTypes.array
+};
+
+export default DocumentList;
