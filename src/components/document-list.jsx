c64bb40545667ed0944b55c72741c65ee5e2f4fb
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-14 00:46:24 +0100

    Display date field on document card

--- src/components/document-list.jsx
+++ src/components/document-list.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
 import { Mutation } from 'react-apollo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { stripHtmlTags } from '../util';
+import { stripHtmlTags, standardizeDate } from '../util';
 import { UserContext } from '../contexts';
 import RenderIconLink from './render-icon-link';
 import { DELETE_DOCUMENT, GET_ALL_DOCUMENTS } from '../queries';
@@ -51,84 +51,87 @@ function DocumentList({ documents }) {
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title text-primary">{document.title}</h5>
-                <p className="card-text">
+                <p className="card-text text-dark font-weight-bold">
                   {renderContentPart(document.content)}
                 </p>
-                {isDocOwner ? (
-                  <span className="d-flex float-right">
-                    <Mutation
-                      mutation={DELETE_DOCUMENT}
-                      onCompleted={data => {
-                        console.log('delete data', data);
-                      }}
-                      refetchQueries={() => [
-                        {
-                          query: GET_ALL_DOCUMENTS
-                        }
-                      ]}
-                      onError={error => {
-                        console.log('delete error', error.message);
-                      }}
-                    >
-                      {mutate => (
-                        <FontAwesomeIcon
-                          icon="trash-alt"
-                          className="fa-2x"
-                          style={styles.deleteIcon}
-                          data-toggle="tooltip"
-                          title="Delete Document"
-                          onClick={() => {
-                            swalWithBootstrapButtons
-                              .fire({
-                                title: 'Are you sure?',
-                                text: "You won't be able to revert this!",
-                                type: 'warning',
-                                showCancelButton: true,
-                                confirmButtonText: 'Yes, delete',
-                                cancelButtonText: 'No, cancel!',
-                                reverseButtons: true
-                              })
-                              .then(async result => {
-                                if (result.value) {
-                                  await mutate({
-                                    variables: {
-                                      id: document.id
-                                    }
-                                  });
-                                  swalWithBootstrapButtons.fire({
-                                    title: 'Document deleted',
-                                    type: 'success',
-                                    timer: 1500,
-                                    showConfirmButton: false,
-                                    toast: true
-                                  });
-                                } else if (
-                                  result.dismiss === Swal.DismissReason.cancel
-                                ) {
-                                  swalWithBootstrapButtons.fire({
-                                    position: 'top-end',
-                                    title: 'Document will not be deleted',
-                                    type: 'info',
-                                    timer: 1000,
-                                    showConfirmButton: false,
-                                    toast: true
-                                  });
-                                }
-                              });
-                          }}
-                        />
-                      )}
-                    </Mutation>
-                    <RenderIconLink
-                      document={document}
-                      iconStyle={styles.editIcon}
-                      iconClass="fa-2x ml-4"
-                      icon="edit"
-                      iconTitleTip="Edit Document"
-                    />
-                  </span>
-                ) : (
-                  <span className="float-right">
+                <span className="d-flex justify-content-between align-items-center">
+                  <p className="font-italic text-muted m-0">
+                    {`Created on ${standardizeDate(document.createdAt)}`}
+                  </p>
+                  {isDocOwner ? (
+                    <span className="d-flex">
+                      <Mutation
+                        mutation={DELETE_DOCUMENT}
+                        onCompleted={data => {
+                          console.log('delete data', data);
+                        }}
+                        refetchQueries={() => [
+                          {
+                            query: GET_ALL_DOCUMENTS
+                          }
+                        ]}
+                        onError={error => {
+                          console.log('delete error', error.message);
+                        }}
+                      >
+                        {mutate => (
+                          <FontAwesomeIcon
+                            icon="trash-alt"
+                            className="fa-2x"
+                            style={styles.deleteIcon}
+                            data-toggle="tooltip"
+                            title="Delete Document"
+                            onClick={() => {
+                              swalWithBootstrapButtons
+                                .fire({
+                                  title: 'Are you sure?',
+                                  text: "You won't be able to revert this!",
+                                  type: 'warning',
+                                  showCancelButton: true,
+                                  confirmButtonText: 'Yes, delete',
+                                  cancelButtonText: 'No, cancel!',
+                                  reverseButtons: true
+                                })
+                                .then(async result => {
+                                  if (result.value) {
+                                    await mutate({
+                                      variables: {
+                                        id: document.id
+                                      }
+                                    });
+                                    swalWithBootstrapButtons.fire({
+                                      title: 'Document deleted',
+                                      type: 'success',
+                                      timer: 1500,
+                                      showConfirmButton: false,
+                                      toast: true
+                                    });
+                                  } else if (
+                                    result.dismiss === Swal.DismissReason.cancel
+                                  ) {
+                                    swalWithBootstrapButtons.fire({
+                                      position: 'top-end',
+                                      title: 'Document will not be deleted',
+                                      type: 'info',
+                                      timer: 1000,
+                                      showConfirmButton: false,
+                                      toast: true
+                                    });
+                                  }
+                                });
+                            }}
+                          />
+                        )}
+                      </Mutation>
+                      <RenderIconLink
+                        document={document}
+                        iconStyle={styles.editIcon}
+                        iconClass="fa-2x ml-4"
+                        icon="edit"
+                        iconTitleTip="Edit Document"
+                      />
+                    </span>
+                  ) : (
                     <RenderIconLink
                       document={document}
                       iconStyle={styles.openIcon}
@@ -136,8 +139,8 @@ function DocumentList({ documents }) {
                       icon="folder-open"
                       iconTitleTip="Open Document"
                     />
-                  </span>
-                )}
+                  )}
+                </span>
               </div>
             </div>
           </div>
