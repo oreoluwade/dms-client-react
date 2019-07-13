3757b3e1bc0f759017824a9bbbee60a82b88d63e
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-14 00:47:02 +0100

    render RenderDocument in document mutation pages

--- src/components/create-document-page.jsx
+++ src/components/create-document-page.jsx
@@ -1,22 +1,10 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
-import Swal from 'sweetalert2';
 import PropTypes from 'prop-types';
-import { Mutation } from 'react-apollo';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { CREATE_DOCUMENT, GET_ALL_DOCUMENTS } from '../queries';
+import { CREATE_DOCUMENT } from '../queries';
+import RenderDocument from './render-document';
 
-const styles = {
-  editorContainer: {
-    height: '70vh'
-  },
-  submitButton: {
-    width: '10vw'
-  }
-};
-
-function CreateDocumentPage({ history }) {
+function CreateDocumentPage() {
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
   const [access, setAccess] = useState('PUBLIC');
@@ -37,92 +25,26 @@ function CreateDocumentPage({ history }) {
   };
 
   return (
-    <div
-      className="d-flex flex-column mr-auto ml-auto mt-5 w-75 bg-light"
-      style={styles.editorContainer}
-    >
-      <div className="row pl-0 pr-0">
-        <div className="col-9 pr-0">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Document Title..."
-            aria-label="Title"
-            aria-describedby="document title"
-            value={title}
-            onChange={handleTitleChange}
-          />
-        </div>
-        <div className="col">
-          <select
-            className="custom-select col"
-            id="inputGroupSelect"
-            value={access}
-            onChange={handleAccessChange}
-          >
-            <option defaultValue>Select Document Access Level</option>
-            <option value="PUBLIC">PUBLIC</option>
-            <option value="PRIVATE">PRIVATE</option>
-            <option value="ROLE">ROLE</option>
-          </select>
-        </div>
-      </div>
-      <CKEditor
-        editor={ClassicEditor}
-        data={content}
-        onInit={editor => {
-          // "editor" available when needed.
-          console.log('Editor is ready to use!', editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setContent(data);
-        }}
-      />
-      <Mutation
-        mutation={CREATE_DOCUMENT}
-        onCompleted={async () => {
-          await resetFields();
-          Swal.fire({
-            position: 'top-end',
-            type: 'success',
-            title: 'Document saved',
-            showConfirmButton: false,
-            timer: 1500,
-            toast: true
-          });
-          history.push('/documents');
-        }}
-        refetchQueries={() => [
-          {
-            query: GET_ALL_DOCUMENTS
-          }
-        ]}
-        onError={error => {
-          console.log('error', error);
-        }}
-      >
-        {mutate => (
-          <button
-            type="button"
-            className="btn btn-primary btn-lg mt-3 ml-auto mr-3"
-            style={styles.submitButton}
-            onClick={async event => {
-              event.preventDefault();
-              await mutate({
-                variables: {
-                  title,
-                  content,
-                  access
-                }
-              });
-            }}
-          >
-            SAVE DOCUMENT
-          </button>
-        )}
-      </Mutation>
-    </div>
+    <RenderDocument
+      alertTitle="Document Saved!"
+      onCompletedCallback={resetFields}
+      handleAccessChange={handleAccessChange}
+      handleTitleChange={handleTitleChange}
+      documentTitle={title}
+      documentAccessType={access}
+      documentBody={content}
+      mutation={CREATE_DOCUMENT}
+      handleDocumentDataChange={(event, editor) => {
+        const data = editor.getData();
+        setContent(data);
+      }}
+      variables={{
+        title,
+        content,
+        access
+      }}
+      mutationType="SAVE DOCUMENT"
+    />
   );
 }
 
---
+++ src/components/update-document-page.jsx
@@ -0,0 +1,77 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { useQuery } from 'react-apollo-hooks';
+import { GET_ONE_DOCUMENT, UPDATE_DOCUMENT } from '../queries';
+import { UserContext } from '../contexts';
+import RenderDocument from './render-document';
+
+function UpdateDocument({ match }) {
+  const { user } = useContext(UserContext);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [access, setAccess] = useState('');
+  const [isDocumentOwner, setIsDocumentOwner] = useState(false);
+
+  const { data, loading, error } = useQuery(GET_ONE_DOCUMENT, {
+    variables: {
+      id: match.params.documentId
+    },
+    skip: !match
+  });
+
+  useEffect(() => {
+    if (user && data && data.getDocument) {
+      setTitle(data.getDocument.title);
+      setContent(data.getDocument.content);
+      setAccess(data.getDocument.access);
+      setIsDocumentOwner(data.getDocument.owner.id === user.id);
+    }
+  }, [user, data]);
+
+  const handleTitleChange = e => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  const handleAccessChange = e => {
+    e.preventDefault();
+    setAccess(e.target.value);
+  };
+
+  if (error) {
+    return <h1>The document could not be retrieved!</h1>;
+  } else {
+    return loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <RenderDocument
+        alertTitle="Document Updated!"
+        isDocumentOwner={isDocumentOwner}
+        handleAccessChange={handleAccessChange}
+        handleTitleChange={handleTitleChange}
+        documentTitle={title}
+        documentAccessType={access}
+        documentBody={content}
+        mutation={UPDATE_DOCUMENT}
+        handleDocumentDataChange={(event, editor) => {
+          const data = editor.getData();
+          setContent(data);
+        }}
+        variables={{
+          title,
+          content,
+          id: match.params.documentId,
+          access
+        }}
+        mutationType="UPDATE"
+      />
+    );
+  }
+}
+
+UpdateDocument.propTypes = {
+  match: PropTypes.object
+};
+
+export default withRouter(UpdateDocument);
