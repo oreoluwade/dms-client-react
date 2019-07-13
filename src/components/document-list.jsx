4429e86ff09fd773e6b1b20c509a92f8a7208612
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-13 09:02:17 +0100

    Add alerts for document actions

--- src/components/document-list.jsx
+++ src/components/document-list.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import Swal from 'sweetalert2';
 import { Mutation } from 'react-apollo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { stripHtmlTags } from '../util';
 import { UserContext } from '../contexts';
 import RenderIconLink from './render-icon-link';
 import { DELETE_DOCUMENT, GET_MY_DOCUMENTS } from '../queries';
+
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+});
 
 const styles = {
   openDocLink: {
@@ -68,12 +77,44 @@ function DocumentList({ documents }) {
                           style={styles.deleteIcon}
                           data-toggle="tooltip"
                           title="Delete Document"
-                          onClick={async () => {
-                            await mutate({
-                              variables: {
-                                id: document.id
-                              }
-                            });
+                          onClick={() => {
+                            swalWithBootstrapButtons
+                              .fire({
+                                title: 'Are you sure?',
+                                text: "You won't be able to revert this!",
+                                type: 'warning',
+                                showCancelButton: true,
+                                confirmButtonText: 'Yes, delete',
+                                cancelButtonText: 'No, cancel!',
+                                reverseButtons: true
+                              })
+                              .then(async result => {
+                                if (result.value) {
+                                  await mutate({
+                                    variables: {
+                                      id: document.id
+                                    }
+                                  });
+                                  swalWithBootstrapButtons.fire({
+                                    title: 'Document deleted',
+                                    type: 'success',
+                                    timer: 1500,
+                                    showConfirmButton: false,
+                                    toast: true
+                                  });
+                                } else if (
+                                  result.dismiss === Swal.DismissReason.cancel
+                                ) {
+                                  swalWithBootstrapButtons.fire({
+                                    position: 'top-end',
+                                    title: 'Document will not be deleted',
+                                    type: 'info',
+                                    timer: 1000,
+                                    showConfirmButton: false,
+                                    toast: true
+                                  });
+                                }
+                              });
                           }}
                         />
                       )}
