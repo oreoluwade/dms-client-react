df205093d2caa54caa9a9f3d8af37afff8c84540
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-13 04:53:50 +0100

    Update document appearance in doc list

--- src/components/document-list.jsx
+++ src/components/document-list.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { stripHtmlTags } from '../util';
+import { UserContext } from '../contexts';
+import RenderIconLink from './render-icon-link';
+
+const styles = {
+  openDocLink: {
+    textDecoration: 'none'
+  },
+  openIcon: {
+    color: 'black'
+  },
+  deleteIcon: {
+    color: 'red'
+  },
+  editIcon: {
+    color: 'green'
+  }
+};
 
 const renderContentPart = content => {
   const normalizedText = stripHtmlTags(content);
@@ -10,21 +28,52 @@ const renderContentPart = content => {
 };
 
 function DocumentList({ documents }) {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="row mt-5">
-      {documents.map(document => (
-        <div className="col-sm-4 mb-2" key={document.id}>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title text-primary">{document.title}</h5>
-              <p className="card-text">{renderContentPart(document.content)}</p>
-              <a href="#" className="btn btn-primary">
-                See more
-              </a>
+      {documents.map(document => {
+        const isDocOwner = user.id === document.owner.id;
+
+        return (
+          <div className="col-sm-4 mb-2" key={document.id}>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title text-primary">{document.title}</h5>
+                <p className="card-text">
+                  {renderContentPart(document.content)}
+                </p>
+                {isDocOwner ? (
+                  <span className="d-flex float-right">
+                    <FontAwesomeIcon
+                      icon="trash-alt"
+                      className="fa-2x"
+                      style={styles.deleteIcon}
+                      data-toggle="tooltip"
+                      title="Delete Document"
+                    />
+                    <RenderIconLink
+                      document={document}
+                      iconStyle={styles.editIcon}
+                      iconClass="fa-2x ml-4"
+                      icon="edit"
+                      iconTitleTip="Edit Document"
+                    />
+                  </span>
+                ) : (
+                  <RenderIconLink
+                    document={document}
+                    iconStyle={styles.openIcon}
+                    iconClass="fa-2x"
+                    icon="folder-open"
+                    iconTitleTip="Open Document"
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
