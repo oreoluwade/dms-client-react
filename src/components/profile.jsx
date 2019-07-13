d57a99fcc39f85fd94e1745da6ce6aed7d5e206c
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-13 08:23:56 +0100

    center profile card

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+const styles = {
+  wrapper: {
+    height: '70vh'
+  }
+};
+
 function Profile() {
-  return <div>Show profile</div>;
+  return (
+    <div
+      className="container d-flex bg-light mt-5 mb-5 justify-content-center"
+      style={styles.wrapper}
+    >
+      <div className="card w-75">
+        <div className="card-body">
+          <h5 className="card-title">Profile</h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
