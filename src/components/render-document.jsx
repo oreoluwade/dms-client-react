bae4e105b48f86212f8a53c6c09c0688fdb6a4ce
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-14 00:45:19 +0100

    create RenderDocument component

--- src/components/render-document.jsx
+++ src/components/render-document.jsx
@@ -1,17 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
 import { withRouter } from 'react-router-dom';
 import { Mutation } from 'react-apollo';
-import { useQuery } from 'react-apollo-hooks';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import {
-  GET_ONE_DOCUMENT,
-  UPDATE_DOCUMENT,
-  GET_ALL_DOCUMENTS
-} from '../queries';
-import { UserContext } from '../contexts';
+import { GET_ALL_DOCUMENTS } from '../queries';
 
 const styles = {
   editorContainer: {
@@ -19,150 +13,139 @@ const styles = {
   },
   submitButton: {
     width: '10vw'
+  },
+  titleField: {
+    height: '6vh'
   }
 };
 
-function RenderDocument({ match, history }) {
-  const { user } = useContext(UserContext);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [access, setAccess] = useState('');
-  const [isDocumentOwner, setIsDocumentOwner] = useState(false);
+function RenderDocument({
+  history,
+  location,
+  alertTitle,
+  isDocumentOwner,
+  handleAccessChange,
+  handleTitleChange,
+  documentTitle,
+  documentAccessType,
+  documentBody,
+  mutation,
+  handleDocumentDataChange,
+  mutationType,
+  variables,
+  onCompletedCallback
+}) {
+  const isCreateDocumentPage = location.pathname === '/create-document';
 
-  const { data, loading, error } = useQuery(GET_ONE_DOCUMENT, {
-    variables: {
-      id: match.params.documentId
-    },
-    skip: !match
-  });
-
-  useEffect(() => {
-    if (user && data && data.getDocument) {
-      setTitle(data.getDocument.title);
-      setContent(data.getDocument.content);
-      setAccess(data.getDocument.access);
-      setIsDocumentOwner(data.getDocument.owner.id === user.id);
-    }
-  }, [user, data]);
-
-  const handleTitleChange = e => {
-    e.preventDefault();
-    setTitle(e.target.value);
-  };
-
-  const handleAccessChange = e => {
-    e.preventDefault();
-    setAccess(e.target.value);
-  };
-
-  if (error) {
-    return <h1>The document could not be retrieved!</h1>;
-  } else {
-    return loading ? (
-      <h1>Loading...</h1>
-    ) : (
-      <div
-        className="d-flex flex-column mr-auto ml-auto mt-5 w-75 bg-light"
-        style={styles.editorContainer}
-      >
-        <div className="row pl-0 pr-0">
-          <div className="col-9 pr-0">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Document Title..."
-              aria-label="Title"
-              aria-describedby="document title"
-              value={title}
-              onChange={handleTitleChange}
-              disabled={!isDocumentOwner}
-            />
-          </div>
-          <div className="col">
-            <select
-              className="custom-select col"
-              id="inputGroupSelect"
-              value={access}
-              onChange={handleAccessChange}
-              disabled={!isDocumentOwner}
-            >
-              <option defaultValue>Select Document Access Level</option>
-              <option value="PUBLIC">PUBLIC</option>
-              <option value="PRIVATE">PRIVATE</option>
-              <option value="ROLE">ROLE</option>
-            </select>
-          </div>
+  return (
+    <div
+      className="container d-flex flex-column mr-auto ml-auto mt-5 bg-light"
+      style={styles.editorContainer}
+    >
+      <div className="row pl-0 pr-0">
+        <div className="col-9 pr-0">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Document Title..."
+            aria-label="Title"
+            aria-describedby="document title"
+            value={documentTitle}
+            onChange={handleTitleChange}
+            disabled={!isDocumentOwner && !isCreateDocumentPage}
+            style={styles.titleField}
+          />
         </div>
-        <CKEditor
-          editor={ClassicEditor}
-          data={content}
-          onInit={editor => {
-            // "editor" available when needed.
-            console.log('Editor is ready to use!', editor);
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            setContent(data);
-          }}
-          disabled={!isDocumentOwner}
-        />
-        {isDocumentOwner ? (
-          <Mutation
-            mutation={UPDATE_DOCUMENT}
-            onCompleted={() => {
-              Swal.fire({
-                position: 'top-end',
-                type: 'success',
-                title: 'Document updated!',
-                showConfirmButton: false,
-                timer: 1500,
-                toast: true
-              });
-              history.push('/documents');
-            }}
-            refetchQueries={() => [
-              {
-                query: GET_ALL_DOCUMENTS
-              }
-            ]}
-            onError={error => {
-              console.log('error', error.message);
-            }}
+        <div className="col">
+          <select
+            className="custom-select col"
+            id="inputGroupSelect"
+            value={documentAccessType}
+            onChange={handleAccessChange}
+            disabled={!isDocumentOwner && !isCreateDocumentPage}
+            style={styles.titleField}
           >
-            {mutate => (
-              <button
-                type="button"
-                className="btn btn-primary btn-lg mt-3 ml-auto mr-3"
-                style={styles.submitButton}
-                onClick={async event => {
-                  event.preventDefault();
-                  await mutate({
-                    variables: {
-                      title,
-                      content,
-                      id: match.params.documentId,
-                      access
-                    }
-                  });
-                }}
-              >
-                UPDATE
-              </button>
-            )}
-          </Mutation>
-        ) : (
-          <span className="mr-3 ml-auto font-italic text-info">
-            This document is Read only
-          </span>
-        )}
+            <option defaultValue>Select Document Access Level</option>
+            <option value="PUBLIC">PUBLIC</option>
+            <option value="PRIVATE">PRIVATE</option>
+            <option value="ROLE">ROLE</option>
+          </select>
+        </div>
       </div>
-    );
-  }
+      <CKEditor
+        editor={ClassicEditor}
+        data={documentBody}
+        onInit={editor => {
+          // "editor" available when needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={handleDocumentDataChange}
+        disabled={!isDocumentOwner && !isCreateDocumentPage}
+      />
+      {isDocumentOwner || isCreateDocumentPage ? (
+        <Mutation
+          mutation={mutation}
+          onCompleted={() => {
+            if (onCompletedCallback) {
+              onCompletedCallback();
+            }
+            Swal.fire({
+              position: 'top-end',
+              type: 'success',
+              title: alertTitle,
+              showConfirmButton: false,
+              timer: 1500,
+              toast: true
+            });
+            history.push('/documents');
+          }}
+          refetchQueries={() => [
+            {
+              query: GET_ALL_DOCUMENTS
+            }
+          ]}
+          onError={error => {
+            console.log('error', error.message);
+          }}
+        >
+          {mutate => (
+            <button
+              type="button"
+              className="btn btn-primary btn-lg mt-3 ml-auto mr-0"
+              style={styles.submitButton}
+              onClick={async event => {
+                event.preventDefault();
+                await mutate({ variables });
+              }}
+            >
+              {mutationType}
+            </button>
+          )}
+        </Mutation>
+      ) : (
+        <span className="mr-3 ml-auto font-italic text-info">
+          This document is Read only
+        </span>
+      )}
+    </div>
+  );
 }
 
 RenderDocument.propTypes = {
-  match: PropTypes.object,
-  history: PropTypes.object
+  history: PropTypes.object,
+  alertTitle: PropTypes.string,
+  isDocumentOwner: PropTypes.bool,
+  handleAccessChange: PropTypes.func.isRequired,
+  handleTitleChange: PropTypes.func.isRequired,
+  documentTitle: PropTypes.string,
+  documentAccessType: PropTypes.string,
+  documentBody: PropTypes.string,
+  mutation: PropTypes.object,
+  handleDocumentDataChange: PropTypes.func.isRequired,
+  mutationType: PropTypes.string,
+  variables: PropTypes.object,
+  location: PropTypes.object,
+  onCompletedCallback: PropTypes.func
 };
 
 export default withRouter(RenderDocument);
