334756e7051f9ea1646b6f0b1e9a910d0d2527b3
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-14 14:20:48 +0100

    specify proper favicon path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(favicon(__dirname + '/dist/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
