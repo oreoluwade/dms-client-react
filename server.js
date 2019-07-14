4c29f2f54bde9f89271a82cb5add1391b5b1c048
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-07-14 14:28:05 +0100

    use compression in production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+const compression = require('compression');
 
 const port = process.env.PORT || 8080;
 
@@ -10,6 +11,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(compression());
 
 app.get('/ping', function(req, res) {
   return res.send('pong');
